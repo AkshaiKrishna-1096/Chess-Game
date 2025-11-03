7c93fc675974297d77101ca166c5b44007121778
tkname2 <nandanatk2@gmail.com>
2025-11-03 23:50:03 +0530

    edited comments.

--- include/pieces/Piece.h
+++ include/pieces/Piece.h
@@ -1,9 +1,3 @@
-// ============================================================================
-// File: include/pieces/Piece.h
-// Description: Abstract base class for all chess pieces
-// Demonstrates: Abstraction, Polymorphism, Inheritance
-// ============================================================================
-
 #ifndef PIECE_H
 #define PIECE_H
 
@@ -12,7 +6,6 @@
 #include <vector>
 #include <string>
 
-// Forward declaration
 class Board;
 
 class Piece {
@@ -20,37 +13,28 @@ protected:
     Color color;
     Position position;
     bool hasMoved;
-    
-    // Protected constructor (cannot instantiate abstract class)
     Piece(Color c, const Position& pos);
-
 public:
-    // Virtual destructor (important for polymorphism)
     virtual ~Piece();
     
-    // Pure virtual functions (Abstract interface)
     virtual bool isValidMove(const Position& to, Board& board) = 0;
     virtual std::vector<Position> getPossibleMoves(Board& board) = 0;
     virtual char getSymbol() const = 0;
-    virtual std::string getName() const = 0;
-    
-    // Virtual function with default implementation (can be overridden)
+    virtual std::string getName() const = 0;   
+
     virtual int getValue() const;
-    
-    // Common getters (Encapsulation)
+
     Color getColor() const;
     Position getPosition() const;
     bool getHasMoved() const;
     
-    // Common setters
     void setPosition(const Position& pos);
     void setHasMoved(bool moved);
     
-    // Utility methods
     bool isWhite() const;
     bool isBlack() const;
     bool isSameColor(const Piece* other) const;
     std::string toString() const;
 };
 
-#endif // PIECE_H
+#endif 
